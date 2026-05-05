cbdd9e2f1fc67f526d3453e693226e8beb798f95
justin_liao <justin_liao@gemteks.com>
2026-05-05 11:43:10 +0800

    del Whilte label ATS , add new README.md

--- ATS_docker/robot_script/White_label_ATS-WREQ-130BE/test.sh
+++
@@ -1,3 +0,0 @@
-function_name="113be"
-#robot -i mesh_mlo_test --output report/mesh/output_mesh.xml --log report/mesh/log_mesh.html --report report/mesh/report_mesh.html test_suites/Mesh/01-WIFI7_MLO.robot
-robot -t "Other test enable sshd" --output report/mesh/output_mesh.xml --log report/mesh/log_mesh.html --report report/mesh/report_mesh.html test_suites/tmp/other_test.robot
